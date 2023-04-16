7a43774f8fec6ad5e5062d853f06eb0c464f69ba
tracer-ics2017 <tracer@njuics.org>
2023-04-16 13:17:08 +0000

    >  run 2012176 ics Linux b88d425c14df 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:17:08 up  5:12,  1 user,  load average: 0.01, 0.03, 0.00

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -2,6 +2,8 @@
 #include <x86.h>
 
 #define RTC_PORT 0x48   // Note that this is not standard
+#define I8042_DATA_PORT 0x60
+#define I8042_STATUS_PORT 0x64
 static unsigned long boot_time;
 
 void _ioe_init() {
@@ -9,7 +11,8 @@ void _ioe_init() {
 }
 
 unsigned long _uptime() {
-  return 0;
+  //use time to get time. Not like native.ioe to use "gettimeofday".
+  return inl(RTC_PORT)-boot_time;
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
@@ -22,15 +25,21 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
-  }
+  int i,j;
+  for(i=0; i<h; i++) {
+     for(j=0;j<w;j++){
+	fb[(y+i)*_screen.width+(x+j)]=*(pixels+i*w+j);
+	}
+ }
 }
 
 void _draw_sync() {
 }
 
 int _read_key() {
+  //when keydown,status_port set to 1.
+  if(inb(I8042_STATUS_PORT)){
+	return inl(I8042_DATA_PORT);
+  }
   return _KEY_NONE;
 }
