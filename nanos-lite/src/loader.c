28340589fefb9aa4638f23a36dcaabf9fc0952f9
tracer-ics2017 <tracer@njuics.org>
2023-04-28 05:46:28 +0000

    >  run 2012176 ics Linux b88d425c14df 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:46:28 up  2:33,  0 users,  load average: 0.07, 0.09, 0.04

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -5,6 +5,6 @@ extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
+  //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
