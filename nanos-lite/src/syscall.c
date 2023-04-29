65aa3581fabecfa91eacecf0dccb45c20fed3e68
tracer-ics2017 <tracer@njuics.org>
2023-04-29 05:42:03 +0000

    >  run 2012176 ics Linux b88d425c14df 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:42:03 up 47 min,  0 users,  load average: 0.06, 0.05, 0.01

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,7 +13,7 @@ _RegSet* do_syscall(_RegSet *r) {
       ret_value=1;
       break;
     case SYS_exit:
-      _halt(1);
+      _halt(a[0]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
