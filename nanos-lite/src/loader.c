a9a1ae88907eefc2707d2f85a3bba8654fad2c6c
tracer-ics2017 <tracer@njuics.org>
2023-05-04 14:53:36 +0000

    >  run 2012176 ics Linux b88d425c14df 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:53:36 up 16:04,  0 users,  load average: 0.01, 0.06, 0.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,10 +7,20 @@ extern ssize_t fs_read(int fd, void *buf, size_t len);
 extern size_t fs_filesz(int fd);
 extern int fs_close(int fd);
 
+extern void* new_page(void);
+
 uintptr_t loader(_Protect *as, const char *filename) {
   //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
   int fd=fs_open(filename,0,0);
-  fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
+  int filesize=fs_filesz(fd);
+  void* page_vaddr=DEFAULT_ENTRY, *page_paddr;
+  while(filesize>0){
+    page_paddr=new_page();
+    _map(as,page_vaddr,page_paddr);	
+    fs_read(fd,page_paddr,PGSIZE);
+    page_vaddr+=PGSIZE;
+    filesize-=PGSIZE;
+  }
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
