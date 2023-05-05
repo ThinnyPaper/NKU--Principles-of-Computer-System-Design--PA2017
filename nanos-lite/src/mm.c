013019312f07bc70ec7416c4bc64522b3270f733
tracer-ics2017 <tracer@njuics.org>
2023-05-05 06:31:09 +0000

    >  run 2012176 ics Linux b88d425c14df 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  06:31:09 up 17:34,  1 user,  load average: 0.06, 0.09, 0.04

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -15,29 +15,31 @@ void free_page(void *p) {
 }
 
 /* The brk() system call handler. */
-int mm_brk(uint32_t new_brk) {
-  if(current->cur_brk == 0){
-	  current->cur_brk = current->max_brk = new_brk;
-  }else{
-	  if(new_brk>current->max_brk){
-	    //TODO:map memory region [current->max_brk,new_brk]
-	    //into address space current->as
-	    // it's similar with loader
-	    int new_space = new_brk-PGROUNDUP(current->max_brk);
-      //int new_space = new_brk-current->max_brk;
-
-      //cause max_brk may in the middle of page;
-	    void *cur_vaddr = (void*)PGROUNDUP(current->max_brk);
-	    void *cur_paddr;
-	    while(new_space>0){
-        cur_paddr=new_page();
-	      _map(&current->as,cur_vaddr,cur_paddr);	
-        cur_vaddr+=PGSIZE;
-        new_space-=PGSIZE;
-  	  }
-	    current->max_brk = new_brk;
-	  }
-	  current->cur_brk = new_brk;
+int mm_brk(uint32_t new_brk) { /// didn't understand yet!!!!
+  //map memory region [current->max_brk,new_brk] into addr spacce current->as
+  //new applied cpace is [max_brk,new_brk)
+  if(current->cur_brk==0)//the first
+  {
+    current->cur_brk=current->max_brk=new_brk;
+  }
+  else
+  {
+    if (current->max_brk < new_brk)
+    {
+      uint32_t first = PGROUNDUP(current->max_brk);
+      uint32_t end = PGROUNDDOWN(new_brk);
+      if ((new_brk & 0xfff) == 0) 
+      {
+        end -= PGSIZE;
+      }
+      for (uint32_t va = first; va <= end; va += PGSIZE) 
+      {
+        void* pa = new_page();
+        _map(&(current->as), (void*)va, pa);
+      }
+      current->max_brk = new_brk;
+    }
+    current->max_brk=new_brk;
   }
   return 0;
 }
