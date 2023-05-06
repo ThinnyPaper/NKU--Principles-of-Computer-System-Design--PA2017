23bfd1eab0a12a4df75654e23a8e2c720ea6cdc1
tracer-ics2017 <tracer@njuics.org>
2023-05-06 03:36:04 +0000

    >  compile 2012176 ics Linux b88d425c14df 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:36:04 up  2:43,  1 user,  load average: 0.22, 0.17, 0.14

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -89,7 +89,7 @@ paddr_t page_translate(vaddr_t addr, bool isRead){
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-    Log("out,%d;len:",addr,len);
+    Log("out,%d;len:%d",addr,len);
 
   if((addr&0x0FFF)+len>PG_SIZE){
     //data cross the page boundary
@@ -106,7 +106,11 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
     uint32_t data=low_data + (high_data<<(res<<3));
     return data;
   }else{
+        Log("else,%d;len:%d",addr,len);
+
     paddr_t paddr=page_translate(addr,true);
+              Log("paddr,%d",paddr);
+
     return paddr_read(paddr, len);
   }
 }
