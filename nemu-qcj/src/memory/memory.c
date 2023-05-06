61389b6390ae1914ec1ecb7c34943ebbbafd1cc3
tracer-ics2017 <tracer@njuics.org>
2023-05-06 03:25:03 +0000

    >  run 2012176 ics Linux b88d425c14df 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:25:03 up  2:32,  1 user,  load average: 0.04, 0.13, 0.14

--- nemu-qcj/src/memory/memory.c
+++ nemu-qcj/src/memory/memory.c
@@ -89,7 +89,7 @@ paddr_t page_translate(vaddr_t addr, bool isRead){
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-    Log("out,%d",addr);
+    Log("out,%d;len:",addr,len);
 
   if((addr&0x0FFF)+len>PG_SIZE){
     Log("%d",addr);
